37ebb194f62ae5d0a014ed17e57b8bf8becd48a6
ashu11-01 <writetoashutosh@gmail.com>
2020-09-01 10:36:12 +0530

    Initial Commit

---
+++ src/main/java/com/decisionmethods/mcdm/McdmApplication.java
@@ -0,0 +1,13 @@
+package com.decisionmethods.mcdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decisionmethods/mcdm/McdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decisionmethods.mcdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
